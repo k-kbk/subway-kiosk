79b3cc2838ae8febc21c9283e2da4ae2e5716bb5
hyunjin <tlsguswls319@naver.com>
2023-02-14 15:43:21 +0900

    refactor: 장바구니 데이터 dummy데이터로 수정

--- src/components/cart/Index.jsx
+++ src/components/cart/Index.jsx
@@ -1,10 +1,61 @@
 import ListBox from './ListBox';
 
 export default function Index() {
+  const dummy = [
+    {
+      id: 1,
+      menu: '에그마요 베이컨',
+      bread: '화이트',
+      cheese: '아메리칸 치즈',
+      vegetable: '토마토, 오이, 양파',
+      sauce: '스위트 칠리, 스위트 어니언',
+      add: '없음',
+      price: '5,800',
+    },
+    {
+      id: 2,
+      menu: '에그마요 페퍼로니',
+      bread: '화이트',
+      cheese: '슈레드 치즈',
+      vegetable: '토마토, 오이, 양파',
+      sauce: '스위트 칠리, 스위트 어니언',
+      add: '없음',
+      price: '8,800',
+    },
+    {
+      id: 3,
+      menu: '에그마요 페퍼로니',
+      bread: '화이트',
+      cheese: '슈레드 치즈',
+      vegetable: '토마토, 오이, 양파',
+      sauce: '스위트 칠리, 스위트 어니언',
+      add: '없음',
+      price: '5,800',
+    },
+    {
+      id: 4,
+      menu: '에그마요 페퍼로니',
+      bread: '화이트',
+      cheese: '슈레드 치즈',
+      vegetable: '토마토, 오이, 양파',
+      sauce: '스위트 칠리, 스위트 어니언',
+      add: '없음',
+      price: '5,800',
+    },
+  ];
   return (
     <div>
-      <ListBox />
-      <ListBox />
+      {dummy.map((dum) => (
+        <ListBox
+          menu={dum.menu}
+          bread={dum.bread}
+          cheese={dum.cheese}
+          vegetable={dum.vegetable}
+          sauce={dum.sauce}
+          add={dum.add}
+          price={dum.price}
+        />
+      ))}
     </div>
   );
 }
--- src/components/cart/ListBox.jsx
+++ src/components/cart/ListBox.jsx
@@ -4,7 +4,15 @@ import Minus from '../../assets/minus.svg';
 import Plus from '../../assets/plus.svg';
 import Sandwich from '../../assets/sandwich.svg';
 
-export default function ListBox() {
+export default function ListBox({
+  menu,
+  bread,
+  cheese,
+  vegetable,
+  sauce,
+  add,
+  price,
+}) {
   const [number, setNumber] = useState(0);
 
   const onIncrease = () => {
@@ -16,129 +24,139 @@ export default function ListBox() {
   };
 
   return (
-    <div
-      css={{
-        width: '900px',
-        height: '250px',
-        background: '#FFFFFF',
-        boxShadow: '1px 1px 3px rgba(0, 0, 0, 0.25)',
-        borderRadius: '15px',
-        marginBottom: '30px',
-      }}
-    >
-      <img
-        alt="cancel"
-        css={{
-          float: 'right',
-          marginRight: '20px',
-          marginTop: '15px',
-          '&:hover': {
-            opacity: '50%',
-          },
-        }}
-        src={Cancel}
-      />
+    <div>
       <div
         css={{
-          display: 'flex',
-          position: 'relative',
-          top: '15px',
+          width: '900px',
+          height: '250px',
+          background: '#FFFFFF',
+          boxShadow: '1px 1px 3px rgba(0, 0, 0, 0.25)',
+          borderRadius: '15px',
+          marginBottom: '30px',
         }}
       >
+        <img
+          role="presentation"
+          alt="cancel"
+          css={{
+            float: 'right',
+            marginRight: '20px',
+            marginTop: '15px',
+            '&:hover': {
+              opacity: '50%',
+            },
+          }}
+          src={Cancel}
+        />
         <div
           css={{
-            width: '220px',
-            height: '220px',
-            background: '#f4f4f4',
-            borderRadius: '15px',
-            marginLeft: '20px',
-            marginRight: '50px',
+            display: 'flex',
+            position: 'relative',
+            top: '15px',
           }}
         >
-          <img
-            alt="menu"
-            src={Sandwich}
-            css={{ marginTop: '35px', width: '160px', marginLeft: '30px' }}
-          />
           <div
             css={{
-              textAlign: 'center',
-              fontStyle: 'normal',
-              fontWeight: '700',
-              fontSize: '24px',
-              width: '160px',
-              marginLeft: '30px',
-              marginTop: '10px',
+              width: '220px',
+              height: '220px',
+              background: '#f4f4f4',
+              borderRadius: '15px',
+              marginLeft: '20px',
+              marginRight: '50px',
             }}
           >
-            에그마요 베이컨 싱글
-          </div>
-        </div>
-        <div
-          css={{
-            marginRight: '70px',
-            fontStyle: 'normal',
-            fontWeight: '700',
-            fontSize: '20px',
-            marginTop: '35px',
-          }}
-        >
-          빵 - 화이트 <br />
-          치즈 - 아메리칸 치즈 <br />
-          야채 - 토마토, 오이, 양파, 피클, 할라피뇨 <br />
-          소스 - 스위트 칠리, 스위트 어니언 <br />
-          추가 선택 - 없음
-        </div>
-        <div css={{ marginTop: '20px' }}>
-          <div
-            css={{
-              fontStyle: 'normal',
-              fontWeight: '600',
-              fontSize: '35px',
-              color: 'var(--green)',
-              marginBottom: '100px',
-            }}
-          >
-            5,800원
-          </div>
-          <div css={{ float: 'right', display: 'flex' }}>
             <img
-              role="presentation"
-              onClick={onDecrease}
-              alt="icon"
-              src={Minus}
-              css={{
-                marginRight: '15px',
-                '&:hover': {
-                  opacity: '50%',
-                },
-              }}
+              alt="menu"
+              src={Sandwich}
+              css={{ marginTop: '50px', width: '160px', marginLeft: '30px' }}
             />
             <div
               css={{
-                width: '50px',
-                background: '#F7F7F7',
-                borderRadius: '15px',
                 textAlign: 'center',
-                color: 'var(--black)',
-                fontWeight: 'bold',
-                fontSize: '25px',
+                fontStyle: 'normal',
+                fontWeight: '700',
+                fontSize: '23px',
+                width: '180px',
+                marginLeft: '20px',
+                marginTop: '10px',
               }}
             >
-              {number}
+              {menu}
             </div>
-            <img
-              role="presentation"
-              onClick={onIncrease}
-              alt="icon"
-              src={Plus}
+          </div>
+          <div
+            css={{
+              marginRight: '70px',
+              fontStyle: 'normal',
+              fontWeight: '700',
+              fontSize: '20px',
+              marginTop: '35px',
+            }}
+          >
+            빵 - {bread} <br />
+            치즈 - {cheese} <br />
+            야채 - {vegetable} <br />
+            소스 - {sauce} <br />
+            추가 선택 - {add}
+          </div>
+          <div css={{ marginTop: '20px' }}>
+            <div
               css={{
-                marginLeft: '15px',
-                '&:hover': {
-                  opacity: '50%',
-                },
+                fontStyle: 'normal',
+                fontWeight: '600',
+                fontSize: '35px',
+                color: 'var(--green)',
+                marginBottom: '100px',
+                marginLeft: '40px',
               }}
-            />
+            >
+              {price}원
+            </div>
+            <div
+              css={{
+                float: 'right',
+                display: 'flex',
+                marginLeft: '40px',
+              }}
+            >
+              <img
+                role="presentation"
+                onClick={onDecrease}
+                alt="icon"
+                src={Minus}
+                css={{
+                  marginRight: '15px',
+                  '&:hover': {
+                    opacity: '50%',
+                  },
+                }}
+              />
+              <div
+                css={{
+                  width: '50px',
+                  background: '#F7F7F7',
+                  borderRadius: '15px',
+                  textAlign: 'center',
+                  color: 'var(--black)',
+                  fontWeight: 'bold',
+                  fontSize: '25px',
+                }}
+              >
+                {number}
+              </div>
+              <img
+                role="presentation"
+                onClick={onIncrease}
+                alt="icon"
+                src={Plus}
+                css={{
+                  marginLeft: '15px',
+                  '&:hover': {
+                    opacity: '50%',
+                  },
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
